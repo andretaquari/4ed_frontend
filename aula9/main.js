64f6a8b0d5867f4b61ac0fb5fb3d99bd42e1d3ea
andretaquari <andrenfagundes@gmail.com>
2020-02-15 16:37:08 -0300

    aula9-casa

--- aula9/main.js
+++ aula9/main.js
@@ -1,6 +1,6 @@
 const submission = document.querySelector("form");
 
-submission.addEventListener("submit", function(e){
+submission.addEventListener("submit", function(e) {
 
 
     e.preventDefault();
@@ -8,16 +8,16 @@ submission.addEventListener("submit", function(e){
     const ano = document.querySelector("#year");
 
     const videoId = "GB_S2qFh5lU";
-    
+
     const wrapper = document.querySelector(".video");
 
-   
 
-    if ( (2020 - ano.value) >= 18 ) {
-        wrapper.innerHTML = `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-        document.querySelector(".popup").style.display = "none";
-    } 
+
+    if ((2020 - ano.value) >= 18) {
+        document.querySelector(".video").style.backgroundBlendMode = "multiply";
+        wrapper.innerHTML = `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+    }
 
 
 });
-
